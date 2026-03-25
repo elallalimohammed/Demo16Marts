f2fc66a57e491452248748eb7bea2dc8fca2a351
Mohammed El Alladi <moal@eaaa.dk>
2026-03-25 12:43:45 +0100

    fix

--- OrderApi_MoqTestProject/UnitTestOrderController.cs
+++ OrderApi_MoqTestProject/UnitTestOrderController.cs
@@ -1,11 +1,16 @@
-﻿namespace OrderApi_MoqTestProject
+﻿using Moq;
+using UsersWebApiMoq16_03_2026.Controllers;
+using UsersWebApiMoq16_03_2026.Repositories;
+
+namespace OrderApi_MoqTestProject
 {
     [TestClass]
     public class OrderControllerTest
     {
-        private Mock<IUserService> _userServiceMock;
-        private Mock<IPaymentService> _paymentServiceMock;
-        private Mock<IOrderRepository> _orderRepositoryMock;
+        private Mock<IUserService> _userServiceMock = null!;
+private Mock<IPaymentService> _paymentServiceMock = null!;
+private Mock<IOrderRepository> _orderRepositoryMock = null!;
+private OrderController _controller = null!;
 
         private OrderController _controller;
 
---
+++ UsersWebApiMoq16_03_2026/Repositories/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using UsersWebApiMoq16_03_2026.Models;
+
+namespace UsersWebApiMoq16_03_2026.Repositories
+{
+    public interface IUserService
+    {
+        Task<User> GetUserById(string userId);   // plus the other methods}
+
+    }
+}
