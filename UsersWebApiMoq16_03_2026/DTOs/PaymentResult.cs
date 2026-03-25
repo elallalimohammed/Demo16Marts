39fe7494365a823c652660597047a206725d0120
Mohammed El Alladi <moal@eaaa.dk>
2026-03-25 11:45:18 +0100

    add order files

---
+++ UsersWebApiMoq16_03_2026/DTOs/PaymentResult.cs
@@ -0,0 +1,9 @@
+﻿namespace UsersWebApiMoq16_03_2026.DTOs
+{
+    public class PaymentResult 
+    { 
+        public bool Success { get; set; } 
+        public string Message { get; set; } 
+    }
+
+}
---
+++ UsersWebApiMoq16_03_2026/Repositories/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using UsersWebApiMoq16_03_2026.Models;
+
+namespace UsersWebApiMoq16_03_2026.Repositories
+{
+    public interface IOrderRepository
+    {
+        Task CreateAsync(Order order);
+    }
+}
